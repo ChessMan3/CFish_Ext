e8890cbf50360b7a427f76e67e77b090b00a7748
ChessMan3 <amult@ukr.net>
2017-03-15 18:37:18 +0200

    Introduce lazy evaluation
    After we have taken into account all cheap evaluation
    terms, we check whether the score exceeds a given threshold.
    If this is the case, we return a scaled down evaluation.
    
    STC:
    LLR: 3.35 (-2.94,2.94) [0.00,5.00]
    Total: 12575 W: 2316 L: 2122 D: 8137
    
    LTC:
    LLR: 2.95 (-2.94,2.94) [0.00,5.00]
    Total: 67480 W: 9016 L: 8677 D: 49787
    
    Current version is the one rewritten by ceebo
    further edited by me.
    
    Bench: 5367704

--- src/evaluate.c
+++ src/evaluate.c
@@ -172,6 +172,9 @@ static const int KingAttackWeights[8] = { 0, 0, 78, 56, 45, 11 };
 #define KnightCheck       924
 
 
+// Threshold for lazy evaluation
+const Value LazyEval = (1500);
+
 // eval_init() initializes king and attack bitboards for a given color
 // adding pawn attacks. To be done at the beginning of the evaluation.
 
@@ -710,6 +713,16 @@ INLINE int evaluate_scale_factor(const Pos *pos, EvalInfo *ei, Value eg)
   return sf;
 }
 
+Value lazy_eval(Value mg, Value eg) {
+  
+ if (mg > LazyEval && eg > LazyEval)
+    return  LazyEval + ((mg + eg) / 2 - LazyEval) / 4;
+   
+ else if (mg < -LazyEval && eg < -LazyEval)
+    return -LazyEval + ((mg + eg) / 2 + LazyEval) / 4;
+  
+ return VALUE_ZERO;
+ }
 
 // evaluate() is the main evaluation function. It returns a static evaluation
 // of the position from the point of view of the side to move.
@@ -738,7 +751,13 @@ Value evaluate(const Pos *pos)
   // Probe the pawn hash table
   ei.pi = pawn_probe(pos);
   score += ei.pi->score;
-
+  
+  // We have taken into account all cheap evaluation terms.
+  // If score exceeds a threshold return a lazy evaluation.
+  Value lazy = lazy_eval(mg_value(score), eg_value(score));
+  if (lazy)
+    return pos_stm() == WHITE ? lazy : -lazy;
+  
   // Initialize attack and king safety bitboards.
   ei.attackedBy[WHITE][0] = ei.attackedBy[BLACK][0] = 0;
   ei.attackedBy[WHITE][KING] = attacks_from_king(square_of(WHITE, KING));
