11d5f68e000d90a4cfc167897f63a44346580979
ChessMan3 <amult@ukr.net>
2017-05-18 17:30:18 +0300

    Bonus for pawn sacrifice which create passed pawn
    STC:
    LLR: 2.95 (-2.94,2.94) [0.00,5.00]
    Total: 16752 W: 3141 L: 2944 D: 10667
    
    LTC:
    LLR: 3.34 (-2.94,2.94) [0.00,5.00]
    Total: 33928 W: 4544 L: 4300 D: 25084
    
    Bench: 5639223
    
    Closes #1092

--- src/evaluate.c
+++ src/evaluate.c
@@ -545,7 +545,7 @@ INLINE Score evaluate_passer_pawns(const Pos *pos, EvalInfo *ei, const int Us)
   while (b) {
     Square s = pop_lsb(&b);
 
-    assert(!(pieces_p(PAWN) & forward_bb(Us, s)));
+    assert(!(pieces_p(Them, PAWN) & forward_bb(Us, s + pawn_push(Us))));
 
     bb = forward_bb(Us, s) & (ei->attackedBy[Them][0] | pieces_c(Them));
     score -= HinderPassedPawn * popcount(bb);
@@ -599,9 +599,9 @@ INLINE Score evaluate_passer_pawns(const Pos *pos, EvalInfo *ei, const int Us)
         mbonus += rr + r * 2, ebonus += rr + r * 2;
     } // rr != 0
 
-    // Scale down bonus for candidate passers which need more than one pawn
-    // push to become passed.
-      if (!pawn_passed(pos, Us, s + pawn_push(Us))) 
+    // Scale down bonus for candidate passers which need more than one
+    // pawn push to become passed or have a pawn in front of them.
+      if (!pawn_passed(pos, Us, s + pawn_push(Us)) || (pieces_p(PAWN) & forward_bb(Us, s)))
               mbonus /= 2, ebonus /= 2;
      
     score += make_score(mbonus, ebonus) + PassedFile[file_of(s)];
